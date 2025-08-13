27437db6263fdbcd0ec1d2fbaaa6dfcb24e43709
JustMeForFun <c.verschuren97@hotmail.com>
2025-08-13 15:23:53 +0200

    update main.py, added word_count function

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ frankenstein_path = "books/frankenstein.txt"
 
 # Prints content of get_book_text to console
 def main():
-    print(get_book_text(frankenstein_path))
+    print(f"{word_count(get_book_text(frankenstein_path))} words found in the document")
 
 # Returns the content of a file as a string
 def get_book_text(filepath):
     
     with open(filepath) as f:
         return f.read()
+
+# Counts the amount of words in a given string    
+def word_count(text):
+    
+    word_list = text.split()
+    return len(word_list)
     
 main()
