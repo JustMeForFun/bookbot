2a08af6030c4775905db1830278f4ade1c07cc50
JustMeForFun <c.verschuren97@hotmail.com>
2025-08-13 15:45:55 +0200

    added character count function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Imports the word_count function from stats.py
 from stats import word_count
+from stats import count_characters
 
 # Sets a path for the frankenstein text file 
 frankenstein_path = "books/frankenstein.txt"
@@ -7,6 +8,7 @@ frankenstein_path = "books/frankenstein.txt"
 # Prints content of get_book_text to console
 def main():
     print(f"{word_count(get_book_text(frankenstein_path))} words found in the document")
+    print(count_characters(get_book_text(frankenstein_path)))
 
 # Returns the content of a file as a string
 def get_book_text(filepath):
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def word_count(text):
     
     word_list = text.split()
     return len(word_list)
+
+def count_characters(text):
+
+    characters = {}
+
+    for c in text.lower():
+        if c not in characters:
+            characters[c] = 1
+        else:
+            characters[c] += 1
+    return characters
