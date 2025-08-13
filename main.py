8fa71d4911ef8a4ff13efbf55e5a048808505bf2
JustMeForFun <c.verschuren97@hotmail.com>
2025-08-13 17:26:59 +0200

    Program takes 2 agruments to support multiple books

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from stats import word_count
 from stats import count_characters
 from stats import character_sorter
 
-# Sets a path for the frankenstein text file 
-frankenstein_path = "books/frankenstein.txt"
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 # Prints content of get_book_text to console
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count(get_book_text(frankenstein_path))} total words")
+    print(f"Found {word_count(get_book_text(book_path))} total words")
     print("--------- Character Count -------")
         
-    for c in character_sorter(count_characters(get_book_text(frankenstein_path))):
+    for c in character_sorter(count_characters(get_book_text(book_path))):
         if c["char"].isalpha() == True:
             print(f"{c["char"]}: {c["num"]}")
 
